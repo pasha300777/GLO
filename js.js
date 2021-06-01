20102f83c00bea3841855513f54669608df868af
Pavel Most <pasha300777@gmail.com>
2021-06-02 01:56:22 +0300

    Lesson02 comment

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 let money = 1000;
-let income = "freelance";; 
+let income = "freelance";
 let addExpenses = 'Internet, Taxi, Rent'; 
 let deposit = true;
 let mission = 10000; 
@@ -11,22 +11,37 @@ console.log(typeof income);
 console.log(typeof deposit);
 addExpenses.toLowerCase();
 let arr = addExpenses.split(',')
+
 console.log(arr);
 console.log("Период равен ("+period+") месяцев, "+"Цель заработать ("+ mission +") долларов");
 console.log(budgetDay);
 
 
 // let num = 266219;
+// let str = String(num);
+// let arr2 = str.split('');
+// let sum = 0;
 
-// for(let i=1; i<=arr.length; i++){
-//     num = num * i;
-//     console.log('num is '+arr);
+// for (let i = 0; i < str.length; i++) {
+// 	sum = sum + Number(arr2[i]); //Когда меняю + на * то выдает 0
+// }
 
-//     console.log(num**3);
+// console.log(sum);
+
+
+
+
+// let num = '266219';
+// let arr2 = num.split("");
+// let sum =0;
+// for(let i=1; i<=num.length; i++){
+//     num =num*i;
+//     console.log('num is '+num);
     
 // }   
-   
-// console.log(num);
+
+// //    console.log(num**3);
+// console.log(sum);
 
 
 
