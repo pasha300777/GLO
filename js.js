13fbb06dd10696b08dccfe2e0e282466778b908a
Pavel Most <pasha300777@gmail.com>
2021-06-01 17:48:27 +0300

    -a

--- js.js
+++ js.js
@@ -1,11 +1,25 @@
-const money;
-const income; 
-const addExpenses; 
-const deposit;
-const mission; 
-const period;
+let money;
+let income; 
+let addExpenses;
+let deposit;
+let mission; 
+let period; 
+let budgetDay;
+
+money = 1000;
+income = "freelance"; 
+addExpenses = 'Internet, Taxi, Rent'; 
+deposit = true;
+mission = 10000; 
+period = 12;
+budgetDay = money/30;
+
+console.log(money+', '+income+', '+deposit);
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log("Период равен ("+period+") месяцев, "+"Цель заработать ("+ mission +") долларов");
+console.log(budgetDay);
+// console.log(“”);
 
 
-alert("Test");
 
-console.log('Test3');
