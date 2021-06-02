52b7336c116e147ee779a9ede8af754c76b3afd0
Pavel Most <pasha300777@gmail.com>
2021-06-02 17:13:54 +0300

    Lesson03 commit

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = 1000;
 let income = "freelance";
 let addExpenses = 'Internet, Taxi, Rent'; 
@@ -16,11 +17,30 @@ let arr = addExpenses.split(',')
 console.log(arr);
 console.log(budgetDay);
 
-
-
-
-
-
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+// console.log(money);
+// console.log(addExpenses);
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = +prompt('Введите обязательную статью расходов?');
+let expenses2 = +prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let budgetMonth = Number(money-expenses1-expenses2-amount1-amount2);
+console.log("Месячный бюджет составляет: ("+budgetMonth+")");
+period = Math.ceil(mission/budgetMonth);
+console.log("Цель будет достигнута за ("+period+") месяцев");
+budgetDay =  Math.floor(budgetMonth/30);
+console.log("Дневной бюджет составляет: ("+budgetDay+")");
+if(budgetDay>=1200){
+  console.log('У вас высокий уровень дохода');
+}else if(budgetDay<=1200 && budgetDay>=600){
+  console.log('У вас средний уровень дохода');
+}else if(budgetDay<=600 && budgetDay>=0){
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+}else {
+  console.log('Что то пошло не так');
+}
 
 // let num = 266219;
 // let str = String(num);
