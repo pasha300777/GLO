4446c6e8e29d9449b52886a01b8827f40d470555
Pavel Most <pasha300777@gmail.com>
2021-06-02 12:41:46 +0300

    Lesson02 comment

--- js.js
+++ js.js
@@ -9,11 +9,11 @@ let budgetDay = money/30;
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен ("+period+") месяцев, "+"Цель заработать ("+ mission +") долларов");
 addExpenses.toLowerCase();
 let arr = addExpenses.split(',')
-
 console.log(arr);
-console.log("Период равен ("+period+") месяцев, "+"Цель заработать ("+ mission +") долларов");
 console.log(budgetDay);
 
 
